f288215f48406ea25ed16e1b5c8a8999548037c0
dante <sack_dante@hotmail.com>
2020-11-28 18:40:09 -0300

    chapter 2

--- server/src/services/accounts/typeDefs.js
+++ server/src/services/accounts/typeDefs.js
@@ -3,11 +3,38 @@ import { gql } from 'apollo-server-express'
 const typeDefs = gql`
     type Account @key(fields: "id") {
         id: ID!
+        createdAt: String!
+        email: String
+    }
+
+    input AccountWhereUniqueInput {
+        id: ID!
+    }
+
+    input CreateAccountInput {
         email: String!
+        password: String!
+    }
+
+    input UpdateAccountInput {
+        email: String,
+        newPassword: String,
+        password: String
     }
 
     extend type Query {
+        account(id: ID!): Account!
+        accounts: [Account]
         viewer: Account
+    }
+
+    extend type Mutation {
+        createAccount(data: CreateAccountInput!): Account!
+        deleteAccount(where: AccountWhereUniqueInput!): Boolean!
+        updateAccount(
+            data: UpdateAccountInput!
+            where: AccountWhereUniqueInput!
+        ): Account!
     }
 `
 
