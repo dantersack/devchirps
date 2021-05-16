ff99df6b9697f577b753748cb47f95210fcb2553
dante <sack_dante@hotmail.com>
2021-05-16 18:15:16 -0300

    chapter 4

--- server/src/scripts/authenticateUser.js
+++ server/src/scripts/authenticateUser.js
@@ -1,5 +1,6 @@
 import getToken from '../lib/getToken'
 
+// node -r dotenv/config -r esm src/scripts/authenticateUser.js [email] [password]
 (async () => {
     const [email, password] = process.argv.slice(2)
     const access_token = await getToken(email, password).catch(error => {
